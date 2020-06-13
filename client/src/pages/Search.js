fecca8f349f38da63ce87588f981ba272ac56d79
pamelac08 <pamelac08@gmail.com>
2020-06-13 11:27:49 -0500

    updated some styling

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -27,16 +27,13 @@ class Search extends Component {
 
     let url = this.state.title;
 
-    fetch(
-      "https://www.googleapis.com/books/v1/volumes?q=" +
-        url
-    )
+    fetch("https://www.googleapis.com/books/v1/volumes?q=" + url)
       .then((res) => res.json())
       .then((result) => {
-          this.setState({
-            books: result.items,
-            title: "",
-          });
+        this.setState({
+          books: result.items,
+          title: "",
+        });
       });
   };
 
@@ -48,22 +45,22 @@ class Search extends Component {
     let saveBook = {};
 
     if (book.volumeInfo.imageLinks) {
-    saveBook = {
-      title: book.volumeInfo.title,
-      description: book.volumeInfo.description,
-      authors: book.volumeInfo.authors,
-      image: book.volumeInfo.imageLinks.smallThumbnail,
-      link: book.volumeInfo.infoLink,
-    };
-  } else {
-    saveBook = {
-      title: book.volumeInfo.title,
-      description: book.volumeInfo.description,
-      authors: book.volumeInfo.authors,
-      image: "",
-      link: book.volumeInfo.infoLink,
-    };
-  }
+      saveBook = {
+        title: book.volumeInfo.title,
+        description: book.volumeInfo.description,
+        authors: book.volumeInfo.authors,
+        image: book.volumeInfo.imageLinks.smallThumbnail,
+        link: book.volumeInfo.infoLink,
+      };
+    } else {
+      saveBook = {
+        title: book.volumeInfo.title,
+        description: book.volumeInfo.description,
+        authors: book.volumeInfo.authors,
+        image: "",
+        link: book.volumeInfo.infoLink,
+      };
+    }
 
     console.log("saveBook: ", saveBook);
 
@@ -87,7 +84,7 @@ class Search extends Component {
               // disabled={!(this.state.author && this.state.title)}
               onClick={this.handleFormSubmit}
             >
-              Submit Book
+              Submit
             </FormBtn>
           </form>
         </div>
@@ -100,40 +97,52 @@ class Search extends Component {
                 {this.state.books.map((book) => (
                   <ListItem key={book.id}>
                     <div className="title-div">
-                        <p><strong>{book.volumeInfo.title}</strong></p>
-                        <p>Written by:&nbsp; 
-                          {book.volumeInfo.authors.map((author, index) => {
-                            if (index === book.volumeInfo.authors.length - 1) {
-                              return <span>{author}</span>
-                            } else {
-                               return <span>{author},&nbsp;</span>
-                            }
+                      <p className="title">
+                        <strong>{book.volumeInfo.title}</strong>
+                      </p>
+                      <p className="author">
+                        Written by:&nbsp;
+                        {book.volumeInfo.authors.map((author, index) => {
+                          if (index === book.volumeInfo.authors.length - 1) {
+                            return <span>{author}</span>;
+                          } else {
+                            return <span>{author},&nbsp;</span>;
                           }
-                          )}</p>
-                      </div>
+                        })}
+                      </p>
+                    </div>
 
-                      <div className="card mb-3 image-description-div">
+                    <div className="card mb-3 image-description-div">
                       <div className="row no-gutters">
                         <div className="col-md-4">
-                          {(book.volumeInfo.imageLinks) ?
-                            <img src={book.volumeInfo.imageLinks.smallThumbnail} className="card-img" alt="..." />
-                            : 
-                            <img src="https://via.placeholder.com/150" className="card-img" alt="..." />
-                          }
+                          {book.volumeInfo.imageLinks ? (
+                            <img
+                              src={book.volumeInfo.imageLinks.smallThumbnail}
+                              className="card-img"
+                              alt="..."
+                            />
+                          ) : (
+                            <img
+                              src="https://via.placeholder.com/150"
+                              className="card-img"
+                              alt="..."
+                            />
+                          )}
                         </div>
                         <div className="col-md-8">
                           <div className="card-body">
-                            <p className="card-text">{book.volumeInfo.description}</p>
+                            <p className="card-text">
+                              {book.volumeInfo.description}
+                            </p>
                           </div>
                         </div>
                       </div>
                     </div>
 
-                      <div className="buttons">
-                        <ViewBtn href={book.volumeInfo.infoLink} />
-                        <SaveButton onClick={() => this.saveBook(book)} />
-                      </div>
-
+                    <div className="buttons">
+                      <ViewBtn href={book.volumeInfo.infoLink} />
+                      <SaveButton onClick={() => this.saveBook(book)} />
+                    </div>
                   </ListItem>
                 ))}
               </div>
