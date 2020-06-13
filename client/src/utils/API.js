26a7025520c5a0a801f23d42d070cbd26bdcaf7f
pamelac08 <pamelac08@gmail.com>
2020-06-12 23:41:04 -0500

    correcting API routes for deployment

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -2,15 +2,15 @@ import axios from "axios";
 
 export default {
     getAllBooks: function() {
-        return axios.get("http://localhost:3001/api/books");
+        return axios.get("/api/books");
     },
     getOneBook: function(id) {
-        return axios.get("http://localhost:3001/api/books/" + id);
+        return axios.get("/api/books/" + id);
     },
     saveBook: function(newBook) {
-        return axios.post("http://localhost:3001/api/books", newBook);
+        return axios.post("/api/books", newBook);
     },
     deleteBook: function(id) {
-        return axios.delete("http://localhost:3001/api/books/" + id);
+        return axios.delete("/api/books/" + id);
     }
 }
